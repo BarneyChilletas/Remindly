31188311b83df098ecfcaebf8e0a0f8f65c6b18e
BarneyChilletas <brisbanypuerta@gmail.com>
2025-05-28 19:14:29 -0500

    refactor: change database connection and add new file in .gitignore

--- src/main/java/org/remindly/util/DBConnection.java
+++ src/main/java/org/remindly/util/DBConnection.java
@@ -1,27 +1,44 @@
 package org.remindly.util;
 
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.SQLException;
+
+import java.util.Properties;
 
 public class DBConnection {
     private static Connection conn = null;
-    private static final String dbName = "remindly";
-    private static final String user = "postgres";
-    private static final String pass = "bany2007";
+
 
 
 
     public static Connection connect() {
+        if (conn != null) {
+            return conn;
+        }
+        Properties props = new Properties();
         try {
+            props.load(new FileInputStream("config.properties"));
+            String dbName = props.getProperty("db.name");
+            String dbUser = props.getProperty("db.user");
+            String dbPassword = props.getProperty("db.pass");
+            String port = props.getProperty("db.port");
+            String url = "jdbc:postgresql://" + props.getProperty("db.host") + ":" + port + "/" + dbName;
             Class.forName("org.postgresql.Driver");
-            if (conn == null || conn.isClosed()) {
-                String url = "jdbc:postgresql://localhost:5432/" + dbName;
-                conn = DriverManager.getConnection(url, user, pass);
-            }
+            conn = DriverManager.getConnection(url, dbUser, dbPassword);
 
-        } catch (Exception e) {
-            System.out.print("Error: ");
+        } catch (SQLException e) {
+            System.out.print("Error connecting to PostgreSQL database: ");
+            e.printStackTrace();
+        }catch (IOException e){
+            System.out.print("Error downloading file properties ");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error loading JDBC driver");
             e.printStackTrace();
         }
 
